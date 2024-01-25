2349a5688f4956561e01bb7f614ff69c06abd369
Douglas Oliveira <douglasrkz@gmail.com>
2024-01-25 11:47:01 -0300

    Startando projeto

---
+++ src/main/java/com/douglasoliveira/desafiobackend/domain/product/Product.java
@@ -0,0 +1,24 @@
+package com.douglasoliveira.desafiobackend.domain.product;
+
+import com.douglasoliveira.desafiobackend.domain.category.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String ownerId;
+    private String price;
+    private Category category;
+
+}
